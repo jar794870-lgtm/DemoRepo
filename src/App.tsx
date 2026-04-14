a69418479e4c9b9a3ec63678e7af6ef78ddcf1ff
Khushi Gupta <kg7460502@gmail.com>
2026-04-14 10:01:08 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,9 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+     hello
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+     
         <button
           className="counter"
           onClick={() => setCount((count) => count + 1)}
